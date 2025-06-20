e6c452fd4c58a1cdc29b290540ee992887deae0d
monts31 <s23120178@alumnos.itsur.edu.mx>
2025-06-20 12:50:14 -0600

    Add files via upload

---
+++ Fibonacci/FibonacciForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Fibonacci
+{
+    partial class FibonacciForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnStart = new Button();
+            btnCancel = new Button();
+            numericUpDown1 = new NumericUpDown();
+            textBox1 = new TextBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(159, 124);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(87, 23);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "Start Async";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Location = new Point(289, 124);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(96, 23);
+            btnCancel.TabIndex = 1;
+            btnCancel.Text = "Cancel start";
+            btnCancel.UseVisualStyleBackColor = true;
+            btnCancel.Click += btnCancel_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(110, 66);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(304, 66);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(btnCancel);
+            Controls.Add(btnStart);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private Button btnCancel;
+        private NumericUpDown numericUpDown1;
+        private TextBox textBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
---
+++ Fibonacci/FibonacciForm.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel;
+
+namespace Fibonacci
+{
+    public partial class FibonacciForm : System.Windows.Forms.Form
+    {
+
+
+        public FibonacciForm()
+        {
+            InitializeComponent();
+        }
+        private void InitializeBackgroundWorker()
+        {
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+    }
+}
